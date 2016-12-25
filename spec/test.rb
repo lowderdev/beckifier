83b22a90cefd2d957e63b52c093a3e0a8847a1b1
Logan Lowder <logan.lowder@metova.com>
2016-12-25 10:39:58 -0500

    change test.rb

--- spec/test.rb
+++ spec/test.rb
@@ -1,22 +1,20 @@
-my_method( has_bad_spacing  )
-  my_hash = {also: has_bad_spacing, and: "unnecessary double quotes"}
-  puts my_hash
+
+# should change:
+
+my_method( bad_spacing  )
+  my_hash = {has: bad_spacing, and: "double quotes"}
+  puts "These quotes stay: #{my_hash}"
+  puts "Won't change strings with ' in them"
 end
 
-This is a hash {a:1}
-
-get_name(   person_1  )
-
-{  b: ( 2 )  }
-
-"Here's a thing"
-
-"this is a: #{test}"
-
+tight_hash = {a:1}
+loose_hash = {  b: ( 2 )  }
+my_method(   tight_hash  )
 ""
-
 "#"
-
 "testn"
 
-a "test"asndft
+# shouldn't change:
+
+"Here's a string with interpolation: #{loose_hash}"
+"Here's a string with a single quote in it"
